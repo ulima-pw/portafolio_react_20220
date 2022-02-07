a465139d3dcf401090835b045287e0234dbe662a
Hernan <hernan@jsatch.com>
2022-02-07 15:11:07 -0500

    Implementando formularios

--- components/formulario_login.component.js
+++ components/formulario_login.component.js
@@ -1,4 +1,11 @@
-const FormularioLogin = () => {
+import { useState } from "react"
+
+const FormularioLogin = (props) => {
+
+    // Variables internas (variables de estado) del componente
+    const [txtUsername, setTxtUsername] = useState("hernan")
+    const [txtPassword, setTxtPassword] = useState("123")
+
     return <aside className="col-md-4">
         <div className="card">
             <div className="card-body">
@@ -6,11 +13,11 @@ const FormularioLogin = () => {
                 <form>
                     <div>
                         <label htmlFor="txt_username" className="form-label">Username</label>
-                        <input id="txt_username" type="text" className="form-control" />
+                        <input id="txt_username" type="text" className="form-control" defaultValue={ txtUsername } />
                     </div>
                     <div className="mb-2">
                         <label htmlFor="txt_password" className="form-label">Password</label>
-                        <input id="txt_password" type="password" className="form-control" />
+                        <input id="txt_password" type="password" className="form-control" defaultValue={ txtPassword }/>
                     </div>
                     <button id="butLogin" className="btn btn-primary" type="button">Login</button>
                     <a href="#">Registro</a>
