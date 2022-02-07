f7d0f44d8b9d03924e485c9d63b3076640ee1991
Hernan <hernan@jsatch.com>
2022-02-07 16:08:38 -0500

    Formulario implementado

--- components/formulario_login.component.js
+++ components/formulario_login.component.js
@@ -3,8 +3,23 @@ import { useState } from "react"
 const FormularioLogin = (props) => {
 
     // Variables internas (variables de estado) del componente
-    const [txtUsername, setTxtUsername] = useState("hernan")
-    const [txtPassword, setTxtPassword] = useState("123")
+    const [txtUsername, setTxtUsername] = useState("")
+    const [txtPassword, setTxtPassword] = useState("")
+
+    const txtUsernameOnChange = (event) => {
+        const txtUsernameIngresado = event.target.value
+        setTxtUsername(txtUsernameIngresado)
+    }
+
+    const txtPasswordOnChange = (event) => {
+        setTxtPassword(event.target.value)
+    }
+
+    const butLoginOnClick = () => {
+        console.log(`Username: ${txtUsername}`)
+        console.log(`Password: ${txtPassword}`)
+        
+    }
 
     return <aside className="col-md-4">
         <div className="card">
@@ -13,13 +28,16 @@ const FormularioLogin = (props) => {
                 <form>
                     <div>
                         <label htmlFor="txt_username" className="form-label">Username</label>
-                        <input id="txt_username" type="text" className="form-control" defaultValue={ txtUsername } />
+                        <input id="txt_username" type="text" className="form-control" 
+                               defaultValue={ txtUsername } onChange={ txtUsernameOnChange }/>
                     </div>
                     <div className="mb-2">
                         <label htmlFor="txt_password" className="form-label">Password</label>
-                        <input id="txt_password" type="password" className="form-control" defaultValue={ txtPassword }/>
+                        <input id="txt_password" type="password" className="form-control" 
+                               defaultValue={ txtPassword } onChange={ txtPasswordOnChange }/>
                     </div>
-                    <button id="butLogin" className="btn btn-primary" type="button">Login</button>
+                    <button id="butLogin" className="btn btn-primary" type="button"
+                            onClick={ butLoginOnClick }>Login</button>
                     <a href="#">Registro</a>
                 </form>
             </div>
--- components/lista_proyectos.component.js
+++ components/lista_proyectos.component.js
@@ -1,27 +1,53 @@
+import { useState } from "react"
+
 const ListaProyectos = (props) => {
-    return <main className="col-md-8">
-        <h3>Ranking</h3>
-        <table className="table">
-            <thead>
-                <tr>
-                    <th>Nombre Proyecto</th>
-                    <th>Usuario</th>
-                    <th>Puntaje</th>
-                </tr>
-            </thead>
-            <tbody id="data_proyectos">
-                {
-                    props.proyectos.map((proyecto) => {
-                        return <tr key={ proyecto.nombre }>
-                            <td>{ proyecto.nombre }</td>
-                            <td>{ proyecto.usuario }</td>
-                            <td>{ proyecto.puntaje }</td>
-                        </tr>
-                    } )
-                }
-            </tbody>
-        </table>
-    </main>
+
+    const [seDebeMostrar, setSeDebeMostrar] = useState(true) // variable de estado del componente
+
+    const butOcultarOnClick = () => {
+        console.log("Se hizo click")
+        setSeDebeMostrar(false) // cambiamos la variable de estado seDebeMostrar
+    }
+
+    const butMostrarOnClick = () => {
+        console.log("Se hizo click")
+        setSeDebeMostrar(true) // cambiamos la variable de estado seDebeMostrar
+    }
+
+
+    if (seDebeMostrar) {
+        return <main className="col-md-8">
+            <h3>Ranking</h3>
+            <button type="button" className="btn btn-success" onClick={butOcultarOnClick} >Ocultar</button>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Nombre Proyecto</th>
+                        <th>Usuario</th>
+                        <th>Puntaje</th>
+                    </tr>
+                </thead>
+                <tbody id="data_proyectos">
+                    {
+                        props.proyectos.map((proyecto) => {
+                            return <tr key={ proyecto.nombre }>
+                                <td>{ proyecto.nombre }</td>
+                                <td>{ proyecto.usuario }</td>
+                                <td>{ proyecto.puntaje }</td>
+                            </tr>
+                        } )
+                    }
+                </tbody>
+            </table>
+        </main>
+    }else {
+        return <main className="col-md-8">
+            <h3>Ranking</h3>
+            <button type="button" className="btn btn-success" onClick={butMostrarOnClick} >Mostrar</button>
+            <p>Tabla oculta</p>
+        </main>
+    }
+
 }
 
 export default ListaProyectos
