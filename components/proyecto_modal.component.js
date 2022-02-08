05a14fc64c8c0222332bdfd6c85578a06e1c500b
Hernan <hernan@jsatch.com>
2022-02-08 16:22:57 -0500

    Nuevo proyecto modal creado

---
+++ components/proyecto_modal.component.js
@@ -0,0 +1,69 @@
+import { useState } from "react"
+import { Modal, Button } from "react-bootstrap"
+
+const ProyectoModal = (props) => {
+    const [txtNombreProyecto, setTxtNombreProyecto] = useState("")
+    const [txtUsuario, setTxtUsuario] = useState("")
+    const [txtRating, setTxtRating] = useState(0)
+
+    const txtNombreProyectoOnChange = (event) => {
+        setTxtNombreProyecto(event.target.value)
+    }
+
+    const txtUsuarioOnChange = (event) => {
+        setTxtUsuario(event.target.value)
+    }
+
+    const txtRatingOnChange = (event) => {
+        setTxtRating(event.target.value)
+    }
+
+    const butGuardarOnClick = () => {
+        props.onGuardarProyecto(txtNombreProyecto, txtUsuario, txtRating)
+    }
+
+
+    return <Modal show={ props.mostrar } 
+                onHide={ props.ocultar }>
+        <Modal.Header closeButton>
+            <Modal.Title>Formulario Proyecto</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+            <form>
+                <div>
+                    <label className="form-label">
+                        Nombre de Proyecto
+                    </label>
+                    <input className="form-control"
+                        type="text" defaultValue={ txtNombreProyecto }
+                        onChange={ txtNombreProyectoOnChange }/>
+                </div>
+                <div>
+                    <label className="form-label">
+                        Usuario
+                    </label>
+                    <input className="form-control"
+                        type="text" defaultValue={ txtUsuario }
+                        onChange={ txtUsuarioOnChange }/>
+                </div>
+                <div>
+                    <label className="form-label">
+                        Rating
+                    </label>
+                    <input className="form-control"
+                        type="number" defaultValue={ txtRating }
+                        onChange={ txtRatingOnChange }/>
+                </div>
+            </form>
+        </Modal.Body>
+
+        <Modal.Footer>
+            <Button variant="secondary">Cerrar</Button>
+            <Button variant="primary"
+                onClick={ butGuardarOnClick }>Guardar</Button>
+        </Modal.Footer>
+    </Modal>
+}
+
+export default ProyectoModal
--- pages/main.js
+++ pages/main.js
@@ -1,12 +1,41 @@
+import { useState } from "react"
 import Footer from "../components/footer.component"
 import ListaProyectos from "../components/lista_proyectos.component"
 import MenuNavegacion from "../components/menu_navegacion.component"
+import ProyectoModal from "../components/proyecto_modal.component"
 
 function MainPage() {
+    const [debeMostrarModal, setDebeMostrarModal] = useState(false) 
+
+    const butNuevoOnClick = () => {
+        setDebeMostrarModal(true)
+    }
+
+    const onModalClose = () => {
+        setDebeMostrarModal(false)
+    }
+
+    const guardarProyectoHandler = (nombreProyecto, usuario, rating) => {
+        console.log("Va a encargarse de guarddar un proyecto en la bd")
+        console.log(nombreProyecto)
+        console.log(usuario)
+        console.log(rating)
+        setDebeMostrarModal(false)
+    }
+
     return <div>
         <h1>Main Page</h1>
         <MenuNavegacion />
+        <div className="mt-4">
+            <button className="btn btn-primary" 
+                onClick={ butNuevoOnClick }>
+                Nuevo
+            </button>
+        </div>
+        <ListaProyectos proyectos={[]} />
         <Footer />
+        <ProyectoModal mostrar={ debeMostrarModal } 
+            ocultar={ onModalClose } onGuardarProyecto={ guardarProyectoHandler }/>
     </div>
 }
 
